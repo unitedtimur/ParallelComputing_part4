7783df1ec3b23593f2d19143e26d95c9e5114278
unitedtimur <lihomanov.t@inbox.ru>
2021-03-10 21:38:43 +0700

    Finish

--- src/threadbarrier.h
+++ src/threadbarrier.h
@@ -1,11 +1,23 @@
 #ifndef THREADBARRIER_H
 #define THREADBARRIER_H
 
+#include <mutex>
+#include <condition_variable>
 
-class ThreadBarrier
+class ThreadBarrier final
 {
 public:
-    ThreadBarrier();
+    explicit ThreadBarrier(const size_t &threadCount);
+
+    void wait() noexcept;
+
+private:
+    mutable size_t m_threadCount;
+    size_t m_threadCountWait;
+    size_t m_threadCountFree;
+
+    mutable std::condition_variable m_cv;
+    std::mutex m_mutex;
 };
 
 #endif // THREADBARRIER_H
